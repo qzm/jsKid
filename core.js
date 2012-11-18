441cd043b6542359e8f58cb6279007b56215c49e
qzm <q.hn@163.com>
2012-11-18 11:49:39 +0800

    增加Message、Cache机制

--- core.js
+++ core.js
@@ -1,12 +1,45 @@
-var cache;
-function main(){
-    setInterval(run, 1000 / 30);
-}
-function run(){
-    time = new Date();
-    if (cache!= time.getSeconds()) {
-        var tz = document.getElementById('timeZone');
-        tz.innerHTML = time.toLocaleDateString() + '<br/>' + time.toLocaleTimeString();
-		cache=time.getSeconds();
+var jsGame = {
+    //初始化资源
+    init: function(args){
+        args.call();
+    },
+    //缓存机制
+    Cache: {
+        map: [],
+        set: function(key, value){
+            this.map[key] = value
+        },
+        get: function(key){
+            return this.map[key]
+        },
+        havekey: function(key){
+            return this.get(key) == null ? false : true
+        },
+        remove: function(key){
+            delete this.map[key]
+        }
+    },
+    message: {
+        queue: [],
+        en: function(msg){
+            for (var m in this.queue) 
+                if (m == msg) 
+                    return false;
+            this.queue.push(msg);
+            return true;
+        },
+        de: function(){
+			if(this.queue[0]!=null)
+				return this.queue[0];
+        	return null;
+        }
+    },
+    //动画入口
+    run: function(funtionToRun){
+		if(this.message.queue[0]!=null){
+			this.message.queue[0].call();
+		}
+        setInterval(funtionToRun, 1000 / 30);
     }
 }
+
---
+++ main.js
@@ -0,0 +1,17 @@
+//程序入口
+function main(){
+    //调用核心库
+    $ = jsGame;
+	$.init(function(){
+        $.Cache.set('click', 'Click');
+	});
+    $.run(function(){
+        time = new Date;
+        if ($.Cache.get('timeCache') != time.getSeconds()) {
+            var tz = document.getElementById('timeZone');
+            tz.innerHTML = time.toLocaleDateString() + '<br/>' + time.toLocaleTimeString();
+            $.Cache.set('timeCache', time.getSeconds());
+        }
+        time = new Date();
+    });
+}
