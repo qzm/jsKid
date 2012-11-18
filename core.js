2040c1b5aadee78549fb6e90e7200c13b81fd71a
qzm <q.hn@163.com>
2012-11-18 13:14:19 +0800

    优化性能、兼容

--- core.js
+++ core.js
@@ -1,49 +1,49 @@
 var jsGame = {
-    //初始化资源
-	browser:true,
-    init: function(args){
-        if (window.ActiveXObject) {
-			this.browser=false;
-            alert("Not IE !");
+	//初始化资源
+	browser: true,
+	init: function(args){
+		if (window.ActiveXObject) {
+			this.browser = false;
+			alert("Not IE !");
 			return null;
-        }
-        args.call();
+		}
+		args.call();
 		return null;
-    },
-    //缓存机制
-    Cache: {
-        map: [],
-        set: function(key, value){
-            this.map[key] = value
-        },
-        get: function(key){
-            return this.map[key]
-        },
-        havekey: function(key){
-            return this.get(key) == null ? false : true
-        },
-        remove: function(key){
-            delete this.map[key]
-        }
-    },
+	},
+	//缓存机制
+	Cache: {
+		map: [],
+		set: function(key, value){
+			this.map[key] = value
+		},
+		get: function(key){
+			return this.map[key]
+		},
+		havekey: function(key){
+			return this.get(key) == null ? false : true
+		},
+		remove: function(key){
+			delete this.map[key]
+		}
+	},
 	//消息机制
-    Message: {
-        queue: [],
-        en: function(msg){
-            for (var m in this.queue) 
-                if (m == msg) 
-                    return false;
-            this.queue.push(msg);
-            return true;
-        },
-        de: function(){
-            if (this.queue[0] != null) 
-                return this.queue[0];
-            return null;
-        }
-    },
-    //动画入口
-    run: function(funtionToRun){
+	Message: {
+		queue: [],
+		en: function(msg){
+			for (var m in this.queue) 
+				if (m == msg) 
+					return false;
+			this.queue.push(msg);
+			return true;
+		},
+		de: function(){
+			if (this.queue[0] != null) 
+				return this.queue[0];
+			return null;
+		}
+	},
+	//动画入口
+	run: function(funtionToRun){
 		if (this.browser) {
 			if (this.Message.queue[0] != null) {
 				this.Message.queue[0].call();
@@ -51,6 +51,6 @@ var jsGame = {
 			funtionToRun.call();
 			setInterval(funtionToRun, 1000 / 30);
 		}
-    }
+	}
 }
 
