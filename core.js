de5e7c8f5f58bd7ca338efe761a040ab803a585d
qzm <q.hn@163.com>
2012-11-20 23:23:25 +0800

    dom、时间、日志操作封�

--- core.js
+++ core.js
@@ -1,5 +1,10 @@
 var jsGame = {
 	//初始化资源
+	version:{
+		name:'jsGame',
+		version:'1.0',
+		author:'QZM'
+	},
 	browser: true,
 	init: function(args){
 		if (window.ActiveXObject) {
@@ -39,6 +44,60 @@ var jsGame = {
 		if (this.browser) {
 			setInterval(funtionToRun, 1000 / 30);
 		}
+	},
+	canvas:{
+
+	},
+	//日志封装
+	log:function(args){console.log(args);},
+	error:function(args){console.error(args);},
+	//document 选择封装
+	dom:function(args){
+		if(args[0]){
+			switch(args[0]){
+				case '#' : //getElementById 的封装
+					return document.getElementById(args.substring(1,args.length));
+				case '@' : //getElementsByName 的封装
+					return document.getElementsByName(args.substring(1,args.length));
+				case '$' : //getElementsByTagName 的封装
+					return document.getElementsByTagName(args.substring(1,args.length));
+				case '.' : //getElementsByClass 的封装
+					return document.getElementsByClassName(args.substring(1,args.length));
+				default:return null;
+			}
+		}
+		return null;
+	},
+	//时间封装
+	t:{
+		now:function(){
+			return (new Date()).toTimeString();
+		},
+		today:function(){
+			return (new Date()).toDateString();
+		},
+		sec:function(){
+			return (new Date()).getSecond();
+		}
+	},
+	print_r:function print_r(theObj){
+		var html='';
+		if(theObj.constructor == Array ||theObj.constructor == Object){
+			html=html+("<ul>");
+			for(var p in theObj){
+				if(theObj[p].constructor == Array||theObj[p].constructor == Object){
+					html=html+("<li>["+p+"] => "+typeof(theObj)+"</li>");
+					html=html+("<ul>");
+					print_r(theObj[p]);
+					html=html+("</ul>");
+				} else {
+					html=html+("<li>["+p+"] => "+theObj[p]+"</li>");
+				}
+			}
+			html=html+("</ul>");
+		}
+		return html;
 	}
+
 };
 
--- main.js
+++ main.js
@@ -3,15 +3,17 @@ function main(){
 	//调用核心库
 	$ = jsGame;
 	c = jsGame.Cache;
+	dom=$.dom;
 	$.init(function(){
 		c.set('Counter', 0);
 	});
+	// document.write($.print_r($.version));
 	$.run(function(){
 		time = new Date();
 		if ($.Cache.get('timeCache') != time.getSeconds()) {
-			var tz = document.getElementById('timeZone');
-			tz.innerHTML = time.toLocaleDateString() + '<br/>' + time.toLocaleTimeString();
+			dom('#timeZone').innerHTML = time.toLocaleDateString() + '<br/>' +$.t.now()+'<br/>'+$.print_r($.version);
 			c.set('timeCache', time.getSeconds());
+			$.log(123);
 		}
 		time = new Date();
 	});
